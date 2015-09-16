5a13cec7eaef794bfa44c753338b0a95d41d1610
Albin <Albin@Albin-Dator.lan>
2015-09-16 22:20:46 +0200

    #2 2015-09-16

---
+++ MidSection.inc.php
@@ -0,0 +1,30 @@
+
+<div class="row">
+
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8 mainSection">
+        <div class="row">
+
+            <div class="col-lg-4">
+                <div class="contentBlock1">
+                    <div class="transparantLayer1"></div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="contentBlock2">
+                    <div class="transparantLayer2"></div>
+                </div>               
+            </div>
+            
+            <div class="col-lg-4">
+                <div class="contentBlock3">
+                    <div class="transparantLayer3"></div>
+                </div>                
+            </div>
+            
+        </div>
+    </div>
+    <div class="col-lg-2"></div>
+
+</div>
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# CAJ
+Saker att lägga till på sidan
+
+Nav-bar
+
+1. Gör inget på den än.
+
+MidSection
+
+1. Zoom på blocken när hover över.
+
+2. Två rader till med block.
+
+Footer
+
+1. Vänta med att göra.
